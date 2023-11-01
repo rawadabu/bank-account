7803724d58ae6f398c23d0aae4fe0adaa2a0cc08
rawadabu <rawadabusaleh0@gmail.com>
2023-11-01 16:20:15 +0200

    Connected the two, react and redux store

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 
 import store from "./store";
 
@@ -11,6 +12,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
